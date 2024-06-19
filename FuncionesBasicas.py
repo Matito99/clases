1343632f71fe121c1433fbc9f96dccc9ca212a41
Matito99 <matia.salasa@duocuc.cl>
2024-06-19 09:43:00 -0400

    Add files via upload

---
+++ FuncionesBasicas.py
@@ -0,0 +1,18 @@
+#a=int(input("Escriba un numero: "))
+#b=int(input("Escriba el segundo numero: "))
+#def resta(a,b):
+#    return a-b
+
+#resultado = a-b
+#print("El resultado es", resultado )
+#----------------------------------------------------
+#def resta(a,b):
+#    return a-b
+
+#print("El resultado es", resta (a=30, b=10))
+#----------------------------------------------------
+def funcion():
+    return"Bienvenidos a python"
+
+frase = funcion()
+print(frase)
